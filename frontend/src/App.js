ce044c3fdb781efdc06fb4ee3fc713abcdfa766e
vidhya <vidhyavakkiyil@gmail.com>
2022-05-26 18:11:19 +0530

    Video-05-Create-Git-Repository

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,40 @@
+import data from "./data";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./HomeScreen/HomeScreen";
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="https://reactjs.org">KE</a>
-      </header>
-      <main>List product</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <a href="https://reactjs.org">KE</a>
+        </header>
+        <main>List product</main>
+        <Routes>
+          <Route path="/" element={<HomeScreen />}></Route>
+        </Routes>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt=""></img>
+              </a>
+              <div className="product_text">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>$</strong>
+                  {product.price}
+                </p>
+
+                <button>ADD TO CART</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+const data = {
+  products: [
+    {
+      name: "Nike slim shirt",
+      slug: "Nike slim shirt",
+      category: "shirts",
+      image:
+        "https://n4.sdlcdn.com/imgs/g/6/k/Signature-Black-Regular-Fit-Formal-SDL001285873-1-6de45.jpg",
+
+      price: 120,
+      CountInStoc: 10,
+      brand: "nike",
+      rating: 4.5,
+      numOfReviews: 14,
+      description: "High Quality Product",
+    },
+    {
+      name: "Adidas slim shirt",
+      slug: "Adidas slim shirt",
+      category: "shirts",
+      image:
+        "https://i5.walmartimages.com/asr/70d49a26-d69a-461b-9189-c0018c2b1f85_1.4854edbb8ee1589a932290770cba427b.jpeg",
+      price: 140,
+      CountInStoc: 10,
+      brand: "Adidas",
+      rating: 4.5,
+      numOfReviews: 14,
+      description: "High Quality Product",
+    },
+    {
+      name: "Otto slim shirt",
+      slug: "Otto slim shirt",
+      category: "shirts",
+      image:
+        "https://i5.walmartimages.com/asr/2b4770d0-166d-47e0-b8ad-a04d4e8836f6_1.d44afd19b420cda13c58da42c38e95b9.jpeg",
+      price: 150,
+      CountInStoc: 10,
+      brand: "Otto",
+      rating: 4.5,
+      numOfReviews: 14,
+      description: "High Quality Product",
+    },
+    {
+      name: "Lavie slim shirt",
+      slug: "Lavie slim shirt",
+      category: "shirts",
+      image:
+        "https://i5.walmartimages.com/asr/5cbaf2dc-286d-401c-b792-af55f882dc4b_1.116c46e3e534d0ca6845e0781eb21361.jpeg",
+      price: 220,
+      CountInStoc: 10,
+      brand: "Lavie",
+      rating: 4.5,
+      numOfReviews: 14,
+      description: "High Quality Product",
+    },
+  ],
+};
+export default data;
