d5698c504e61860a8af45c73d72444e625eb1c8c
vidhya <vidhyavakkiyil@gmail.com>
2022-05-26 16:11:42 +0530

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="https://reactjs.org">KE</a>
+      </header>
+      <main>List product</main>
+    </div>
+  );
+}
+
+export default App;
